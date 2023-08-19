d0b4ed1d12d58dbe286838cbf2d40d813d60a991
최희원 <jeeen1@naver.com>
2023-08-19 22:24:37 +0900

    feature: quiz main page

---
+++ app/quiz/[id]/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import * as S from "@/app/styles";
+
+export default function Page() {
+  return <S.Container>quiz id</S.Container>;
+}
---
+++ components/PresetItem.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { MdNavigateNext } from "react-icons/md";
+import styled from "styled-components";
+import { Octagon } from "./octagon";
+
+const RecentPresetItem = styled.div`
+  display: flex;
+  width: 100%;
+  height: 68px;
+  padding: 16px 0;
+  padding-right: 8px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #e2e7e9;
+`;
+
+const RecentPresetTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const RecentPresetTitle = styled.div`
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%;
+  letter-spacing: -0.2px;
+`;
+
+interface PresetItemProps {
+  title: string;
+  backgroundImage: string;
+}
+
+const PresetItem = (props: PresetItemProps) => {
+  const { title, backgroundImage } = props;
+
+  return (
+    <RecentPresetItem key={title}>
+      <RecentPresetTitleContainer>
+        <Octagon width="36px" backgroundImage={backgroundImage}></Octagon>
+        <RecentPresetTitle>{title}</RecentPresetTitle>
+      </RecentPresetTitleContainer>
+      <MdNavigateNext size="24px" />
+    </RecentPresetItem>
+  );
+};
+
+export default PresetItem;
