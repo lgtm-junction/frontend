9e6fec02049de1e5eb65405a110413f89f0b5ce3
Suhyun Park <me@shiftpsh.com>
2023-08-20 03:33:21 +0900

    feat: load menu from backend

--- app/store/[id]/menu/[menuId]/page.tsx
+++ app/store/[id]/menu/[menuId]/page.tsx
@@ -2,68 +2,81 @@
 
 import * as S from "@/app/styles";
 import BottomSheets from "@/components/BottomSheets";
+import CustomList from "@/components/Custom/List";
+import CustomOptionList from "@/components/Custom/OptionList";
 import { Divider } from "@/components/global/Divider";
 import Promotion from "@/components/global/Promotion";
-import { MenuType } from "@/types/type";
-import { useEffect, useState } from "react";
-import CustomOptionList from "@/components/Custom/OptionList";
 import { CUSTOM } from "@/constants/dummy";
-import CustomList from "@/components/Custom/List";
-
-const MENU: MenuType = {
-  id: "1",
-  name: "Cafe Latte",
-  description:
-    "A cafe latte is a milk coffee that is a made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top.",
-  price: 5000,
-  imageUrl: "/cafeLatte.jpeg",
-  customizations: [],
-  options: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-  allergyInfo: ["Nuts"],
-};
+import { MenuCollectionName, getDocument } from "@/firebase/getData";
+import { MenuType } from "@/types/type";
+import { useParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Page() {
+  const { menuId } = useParams();
   const [openedCustom, setOpenedCustom] = useState<string | null>(null);
   const [screenHeight, setScreenHeight] = useState(2000);
-  const [menu, setMenu] = useState<MenuType>();
+  const [menu, setMenu] = useState<MenuType | null>(null);
+
+  const loadMenu = useCallback(async () => {
+    if (typeof menuId !== "string") return;
+    try {
+      const res = await getDocument<MenuType>(MenuCollectionName, menuId);
+      setMenu(res);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [menuId]);
 
   useEffect(() => {
     setScreenHeight(window.innerHeight);
   }, []);
 
+  useEffect(() => {
+    loadMenu();
+  }, [loadMenu]);
+
   return (
     <S.Container>
-      <div className="w-[calc(100%+32px)] h-64 relative -translate-x-4 -mt-4">
-        <img src={MENU.imageUrl} className="w-full h-full object-cover" />
-      </div>
-      <div className="p-1">
-        <div className="flex flex-col items-start">
-          <div className="text-h2 mt-8 mb-2">{MENU.name}</div>
-          <div className="text-small text-gray-500">{MENU.description}</div>
-          <div className="text-strong mt-4 my-2">Allergy</div>
-          <div>{MENU.allergyInfo.join(", ")}</div>
-          <div></div>
-        </div>
-        <Divider />
-        <div className="flex flex-col gap-3">
-          <div className="text-strong mt-4 my-2">Customs</div>
-          <Promotion />
-          <CustomList items={CUSTOM} setOpenedCustom={setOpenedCustom} />
-        </div>
-      </div>
-      <BottomSheets
-        initialTop={openedCustom ? 100 : screenHeight}
-        close={openedCustom ? () => setOpenedCustom(null) : null}
-      >
-        <div className="px-4 py-4">
-          {openedCustom && (
-            <CustomOptionList
-              id={openedCustom}
-              close={() => setOpenedCustom(null)}
-            />
-          )}
-        </div>
-      </BottomSheets>
+      {menu ? (
+        <>
+          <div className="w-[calc(100%+32px)] h-64 relative -translate-x-4 -mt-4">
+            <img src={menu.imageUrl} className="w-full h-full object-cover" />
+          </div>
+          <div className="p-1">
+            <div className="flex flex-col items-start">
+              <div className="text-h2 mt-8 mb-2">{menu.name}</div>
+              <div className="text-small text-gray-500">{menu.description}</div>
+              <div className="text-strong mt-4 my-2">Allergy</div>
+              <div>
+                {menu.allergyInfo.length ? menu.allergyInfo.join(", ") : "None"}
+              </div>
+              <div></div>
+            </div>
+            <Divider />
+            <div className="flex flex-col gap-3">
+              <div className="text-strong mt-4 my-2">Customs</div>
+              <Promotion />
+              <CustomList items={CUSTOM} setOpenedCustom={setOpenedCustom} />
+            </div>
+          </div>
+          <BottomSheets
+            initialTop={openedCustom ? 100 : screenHeight}
+            close={openedCustom ? () => setOpenedCustom(null) : null}
+          >
+            <div className="px-4 py-4">
+              {openedCustom && (
+                <CustomOptionList
+                  id={openedCustom}
+                  close={() => setOpenedCustom(null)}
+                />
+              )}
+            </div>
+          </BottomSheets>
+        </>
+      ) : (
+        <>Loading...</>
+      )}
     </S.Container>
   );
 }
