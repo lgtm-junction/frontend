01c63b7961ee8be4325f2cc3b95c3e89ddb0e0a5
r-4bb1t <r4bb1t.github@gmail.com>
2023-08-19 13:12:26 +0900

    style: bottom sheet style

--- components/BottomSheets/index.tsx
+++ components/BottomSheets/index.tsx
@@ -1,41 +1,15 @@
 "use client";
 
-import Sheet, { SheetRef } from "react-modal-sheet";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode } from "react";
 import * as S from "./styles";
 
-export default function BottomSheets({
-  children,
-  height,
-}: {
-  children: ReactNode;
-  height: number;
-}) {
-  const ref = useRef<SheetRef>();
-  const snapTo = (i: number) => ref.current?.snapTo(i);
-  const [top, setTop] = useState(0);
-
-  useEffect(() => {
-    setTop(window.innerHeight - 60);
-  }, []);
-
+export default function BottomSheets({ children }: { children: ReactNode }) {
   return (
-    <Sheet
-      isOpen={true}
-      onClose={() => {
-        snapTo(1);
-      }}
-      snapPoints={[top, height]}
-      initialSnap={1}
-      ref={ref}
-    >
-      <Sheet.Container>
-        <Sheet.Header />
-        <Sheet.Content>
-          <Sheet.Scroller>{children}ddd</Sheet.Scroller>
-        </Sheet.Content>
-      </Sheet.Container>
-      <S.Backdrop onTap={() => snapTo(1)} />
-    </Sheet>
+    <S.Wrapper>
+      <S.HandleContainer>
+        <S.Handle />
+      </S.HandleContainer>
+      <S.BottomSheetContent>{children}</S.BottomSheetContent>
+    </S.Wrapper>
   );
 }
