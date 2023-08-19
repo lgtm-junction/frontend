a1d88e0ca50afe185102114fa4d76abd79297d88
ritajeong <ritajeong.dev@gmail.com>
2023-08-19 16:27:33 +0900

    style: update divider component margin

--- components/global/Divider.tsx
+++ components/global/Divider.tsx
@@ -1,6 +1,6 @@
 export const Divider = () => {
   return (
-    <div className="w-full h-12 flex justify-center items-center">
+    <div className="w-full h-12 flex justify-center items-center mt-4">
       <hr className="w-24 h-1 bg-gray-300" />
     </div>
   );
