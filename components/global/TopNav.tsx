839758b9d23afafe9155e7749775952509305476
최희원 <jeeen1@naver.com>
2023-08-19 16:00:05 +0900

    feat: add profile icon

--- components/global/TopNav.tsx
+++ components/global/TopNav.tsx
@@ -1,16 +1,18 @@
 import * as S from "@/app/styles";
 import Image from "next/image";
 
+import { MdPerson } from "react-icons/md";
+
 export const TopNav = () => {
   return (
     <div
-      className="h-16 border flex justify-between p-4"
+      className="h-16 border flex justify-between content-center p-4"
       style={{ zIndex: "99999999" }}
     >
       <S.Link href="/">
         <Image width={110} height={29} src="/Logo.svg" alt="romakase" />
       </S.Link>
-      <div>logout?</div>
+      <MdPerson size="32px" />
     </div>
   );
 };
