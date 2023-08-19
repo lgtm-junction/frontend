61a5d6d3f2b60d0051a36d4189898121b201a6fc
ritajeong <ritajeong.dev@gmail.com>
2023-08-20 01:17:35 +0900

    feat: store data binding

--- app/nearby/page.tsx
+++ app/nearby/page.tsx
@@ -2,61 +2,10 @@ import * as S from "@/app/styles";
 import BottomSheets from "@/components/BottomSheets";
 import Map from "@/components/Map";
 import Link from "next/link";
-import Icons from "@/components/Icons";
 import { MdLocationOn } from "react-icons/md";
+import { testRestaurants } from "@/firebase/models/example-models";
 export default function Nearby() {
   const bexcoLatLng = { latitude: 35.1689766, longitude: 129.1360411 };
-  const MENUS = [
-    {
-      id: 1,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-    {
-      id: 2,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-    {
-      id: 3,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-    {
-      id: 4,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-    {
-      id: 5,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-    {
-      id: 6,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-    {
-      id: 7,
-      name: "Cafe Latte",
-      price: 5000,
-      image: "/cafeLatte.jpeg",
-      customOptions: ["Milk Amount", "Grinding", "asdfasdf", "asdfdfs"],
-    },
-  ];
   return (
     <S.Container>
       <div className="relative">
@@ -72,39 +21,26 @@ export default function Nearby() {
       </div>
       <BottomSheets initialTop={350}>
         <div className="w-full flex flex-col px-4">
-          {/* todo: fix href */}
-          {MENUS.map((menu) => (
+          {testRestaurants.map(({ name, geoInformation }, idx) => (
             <Link
               href="/store/1"
               className="flex gap-3 items-center border-b border-b-gray-100 last-of-type:border-b-transparent py-4"
-              key={menu.id}
+              key={`store-${idx}`}
             >
-              <div className="w-20 h-20 border border-black">
-                <img src={menu.image} className="w-full h-full object-cover" />
+              <div className="w-20 h-20 border border-black shrink-0">
+                <img src="/cake.jpg" className="w-full h-full object-cover" />
               </div>
 
               <div className="flex flex-col">
-                <div className="text-strong">{menu.name}</div>
-                <div className="text-p mb-2">
-                  ₩ {menu.price.toLocaleString()}
+                <div className="flex justify-between gap-1">
+                  <div className="text-strong">{name}</div>
+                  <div className="text-gray-400 text-base whitespace-nowrap">
+                    {geoInformation.pseudoDistance} km
+                  </div>
                 </div>
-
                 <div className="flex gap-2 items-center">
-                  <Icons.RobotArm className="w-8 h-8" />
-                  <div>
-                    <div className="text-sm text-gray-500">Custom options</div>
-                    <div className="text-sm">
-                      {menu.customOptions.slice(0, 2).map((option, i) => (
-                        <span
-                          key={i}
-                          className="after:content-[',_'] last-of-type:after:content-['']"
-                        >
-                          {option}
-                        </span>
-                      ))}
-                      {menu.customOptions.length > 2 &&
-                        `, +${menu.customOptions.length - 2}`}
-                    </div>
+                  <div className="text-sm text-gray-500 pt-1">
+                    {geoInformation.address}
                   </div>
                 </div>
               </div>
