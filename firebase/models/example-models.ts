2f1f49fe589ee33409050a09e8ba888515923df4
Suho Lee <bird@susuyo.ai>
2023-08-19 23:54:08 +0900

    feat(model): Update example-models

--- firebase/models/example-models.ts
+++ firebase/models/example-models.ts
@@ -1,39 +1,133 @@
 import { MenuType, RestaurantType } from "@/types/type"
 
+export const testMenus: Omit<MenuType, "id">[] = [
+  {
+  // id: "rbKVdjTkqNAjfXghIDxH"
+    name: "Spiced Chicken",
+    price: 20000,
+    options: ["지코특제양념", "지코특제양념", "설탕", "소금"],
+    customizations: [
+      {
+        id: "1",
+        name: "ZICO YANGNYUM",
+        author: {
+          id: "Gikoba",
+          image: "http://ai.esmplus.com/campcam/top/logo.jpg",
+        },
+        tags: ["ZICO", "Spice"],
+        price: 100,
+        options: [
+          {
+            id: "1",
+            name: "Strength of YANGNYUM",
+            isBoolean: false,
+            unit: "%",
+            min: 1,
+            max: 100,
+            value: 1,
+          },
+        ]
+      }
+    ],
+    allergyInfo: [],
+    imageUrl: "/cafeLatte.jpeg",
+    description: "Gikoba is a Korean chicken brand. It has an exceptionally unique format among brands. A type of chicken cut into small pieces, grilled, stir-fried in spices, and put in a box. Just like tteokbokki, bar rice cakes are also stir-fried, and you can increase the amount of bar rice cakes by adding a fee of 1,000 to 2,000 won. Tteoksari can be added only to seasoned grilled chicken, and it is impossible to add rice cakes to salt-grilled chicken, whether it is boneless or bone-in chicken.",
+  },
+  {
+    name: "Cappuccino",
+    price: 45000,
+    description: "Smooth and rich espresso with a layer of frothy milk.",
+    imageUrl: "/cafeLatte.jpeg",
+    allergyInfo: ["Milk"],
+    customizations: [],
+    options: ["Regular", "Large"],
+  },
+  {
+    name: "Mocha Latte",
+    price: 55000,
+    description: "Espresso and steamed milk with a touch of chocolate.",
+    imageUrl: "/cafeLatte.jpeg",
+    allergyInfo: ["Milk", "Chocolate"],
+    customizations: [
+      {
+        id: "1",
+        name: "Extra Shot",
+        author: {
+          id: "shiftpsh",
+          image: "https://pbs.twimg.com/profile_images/1559136628609732608/hoYcE2w6_400x400.jpg",
+        },
+        tags: ["Espresso"],
+        price: 5000,
+        options: [
+          {
+            id: "1",
+            name: "Add Extra Shot",
+            isBoolean: true,
+            unit: "shot",
+            value: 1,
+          },
+        ],
+      },
+    ],
+  },
+]
 
-export const testRestaurant: Omit<RestaurantType, "id"> = {
-// id: "G91GZDuaAepwXonMlV40"
-  name: "BBQ치킨",
-  geoInformation: {
-    address: "234-9, Centumnam-daero, Haeundae-gu, Busan",
-    marker: {
-      latitude: 35.1600161,
-      longitude: 129.1630504,
+export const testRestaurants: Omit<RestaurantType, "id">[] = [
+  {
+    // id: "G91GZDuaAepwXonMlV40"
+    name: "BBQ Chicken",
+    geoInformation: {
+      address: "234-9, Centumnam-daero, Haeundae-gu, Busan",
+      marker: {
+        latitude: 35.1600161,
+        longitude: 129.1630504,
+      },
+      pseudoDistance: 2.1,
     },
-    pseudoDistance: 2.1,
+    searchKeywords: ["치킨"],
+    robot: {
+      modelName: "JAKA Zu 18",
+      lastBathDateTime: new Date(),
+      comment: "I will serve you delicious chicken!",
+    },
+    menuIds: [
+      "rbKVdjTkqNAjfXghIDxH"
+    ],
   },
-  searchKeywords: ["치킨"],
-  robot: {
-    modelName: "JAKA Zu 18",
-    lastBathDateTime: new Date(),
-    comment: "마쉿는 케이크를 만들어 드릴게욤,,",
+  {
+    name: "BUSAN EXPO 2030 WORLD CAFE",
+    geoInformation: {
+      address: "234-9, Centumnam-daero, Haeundae-gu, Busan",
+      marker: {
+        latitude: 35.1600161,
+        longitude: 129.1630504,
+      },
+      pseudoDistance: 2.1,
+    },
+    searchKeywords: ["카페", "엑스포"],
+    robot: {
+      modelName: "JAKA Zu 12",
+      lastBathDateTime: new Date(),
+      comment: "Korea's challenge to host the World EXPO! Busan begins the challenge, and Korea achieves it together.",
+    },
+    menuIds: [],
   },
-  menuIds: [],
-}
-
-export const testFood: Omit<MenuType, "id"> = {
-// id: "rbKVdjTkqNAjfXghIDxH"
-  name: "황금올리브치킨",
-  basePrice: 20000,
-  customizations: [
-    {
-      id: "zicoyangnyum",
-      name: "ZICO YANGNYUM",
-      additionalPrice: 21000,
-      tags: ["SWEET", "NUTS", "ZICO"],
-      marketingSlug: "지코의 \n지코바보다 맛있는 \n특제양념소스치킨",
-      customizations: ["지코특제양념", "지코특제양념", "설탕", "소금"],
-      author: "zicova",
-    }
-  ]
-}
+  {
+    name: "Hexagon wrench French",
+    geoInformation: {
+      address: "234-9, Centumnam-daero, Haeundae-gu, Busan",
+      marker: {
+        latitude: 35.1600161,
+        longitude: 129.1630504,
+      },
+      pseudoDistance: 2.1,
+    },
+    searchKeywords: ["french"],
+    robot: {
+      modelName: "JAKA Zu 18",
+      lastBathDateTime: new Date(),
+      comment: "Supreme quality French cuisine like Hexagon",
+    },
+    menuIds: [],
+  },
+]
