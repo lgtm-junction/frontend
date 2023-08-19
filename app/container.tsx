14f51773287c8a48def13b1101f0e1cea1d70ad5
Suhyun Park <me@shiftpsh.com>
2023-08-20 04:03:28 +0900

    feat: hide cart in quizzes

--- app/container.tsx
+++ app/container.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import CartButton from "@/components/global/CartButton";
 import { TopNav } from "@/components/global/TopNav";
 import { AlertProvider } from "@/context/useAlert";
 import ContainerRefProvider from "@/context/useContainerRef";
-import StyledComponentsRegistry from "./registry";
-import CartButton from "@/components/global/CartButton";
+import { usePathname } from "next/navigation";
 import { useRef } from "react";
+import StyledComponentsRegistry from "./registry";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   const ref = useRef<HTMLDivElement>(null);
+  const path = usePathname();
+  const showCart =
+    path.match(/^\/(cart|order|nearby|store)(\/.*)?$/g) || path === "/";
 
   return (
     <div className="relative w-full max-w-[420px] min-h-screen h-full m-0 mx-auto flex flex-col flex-1 border">
@@ -16,7 +20,7 @@ export default function Container({ children }: { children: React.ReactNode }) {
         className="fixed top-0 w-full max-w-[420px] h-screen pointer-events-none z-[101]"
         ref={ref}
       >
-        <CartButton />
+        {showCart && <CartButton />}
       </div>
       <ContainerRefProvider containerRef={ref}>
         <AlertProvider>
