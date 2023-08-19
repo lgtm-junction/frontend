bee51e0506f9439e8841e1959f5b55eb99b0b48d
ritajeong <ritajeong.dev@gmail.com>
2023-08-19 11:42:40 +0900

    style: add mobile first layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import StyledComponentsRegistry from "./registry";
 import "./globals.css";
 import "./tailwindcss.css";
+import { TopNav } from "@/components/global/TopNav";
+import { BottomNav } from "@/components/global/BottomNav";
 
 export default function RootLayout({
   children,
@@ -10,7 +12,13 @@ export default function RootLayout({
   return (
     <html data-theme="light">
       <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <div className="w-[420px] h-screen m-0 mx-auto flex flex-col flex-1 border">
+          <StyledComponentsRegistry>
+            <TopNav />
+            <div className="h-full p-4">{children}</div>
+            <BottomNav />
+          </StyledComponentsRegistry>
+        </div>
       </body>
     </html>
   );
