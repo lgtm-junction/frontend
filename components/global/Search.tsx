4f2149a7ecd9a77ed59bdfa15e7d66c35707a439
ritajeong <ritajeong.dev@gmail.com>
2023-08-20 10:52:36 +0900

    chore: remove korean

--- components/global/Search.tsx
+++ components/global/Search.tsx
@@ -33,7 +33,7 @@ export const Search = () => {
     <label className="h-12 w-full bg-gray-100 pl-4 pr-4 py-2 rounded-lg flex gap-2 items-center">
       <input
         className="input w-full bg-none !outline-none bg-opacity-0 px-0"
-        placeholder="뭐가 드시고 싶으신가욤... 로봇이 다 해드랴욤..."
+        placeholder="Enter your search here"
         type="text"
         onKeyDown={handleOnKeyDown}
       />
